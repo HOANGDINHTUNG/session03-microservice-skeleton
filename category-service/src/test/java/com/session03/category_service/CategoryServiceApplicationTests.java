b483cd337122b5e0a9203ec446895a03fc832e95
unknown <mtung3365@gmail.com>
2026-09-08 12:15:10 +0700

    init

---
+++ category-service/src/test/java/com/session03/category_service/CategoryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.session03.category_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = {"app.message=test"})
+class CategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
